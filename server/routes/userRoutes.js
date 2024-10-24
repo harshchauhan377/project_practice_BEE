d280deae1c9e1cd7042366fd32aa53cb0c1bf5c5
hc <hcrajput321@gmail.com>
2024-10-24 13:36:34 +0530

    24-10-2024

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,12 @@
+const express = require("require");
+const router = express.Router();
+
+const{registerUser,loginUser} = require("../controllers/userController");
+
+//route for user registration
+router.post("/register",registerUser);
+
+//route for user login
+router.post("/login",loginUser);
+
+module.exports = router;
